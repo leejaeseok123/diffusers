b82cedf9b9c1b2d2e70c314cfc3126513c3dd708
leejaeseok123 <112832262+leejaeseok123@users.noreply.github.com>
2026-04-30 18:18:25 +0900

    Update SD_v3.5.py

--- SD_v3.5.py
+++ SD_v3.5.py
@@ -65,8 +65,8 @@ coco_annotation_path = "/home/jslee/diffusion_exper/batch_exper/dataset/coco2014
 csv_output_file = "SD3.5_scaling.csv"
 
 total_images = 300
-batch_sizes  = list(range(2, 20, 2))
-step_sizes   = [4, 6, 8, 10, 12, 14, 16, 18, 20, 28, 30, 40]
+batch_sizes  = [1, 2, 4, 8, 16, 32, 64, 80, 96, 128]
+step_sizes   = [4, 6, 8, 10, 12, 14, 16, 18, 20, 30, 40, 50]
 num_runs     = 1
 H, W         = 1024, 1024
 
