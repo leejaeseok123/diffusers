b068bcbe065dca80bf291e80cd6dbaf395cd304f
leejaeseok123 <112832262+leejaeseok123@users.noreply.github.com>
2026-05-04 15:54:03 +0900

    Update SD1.5_FID.py

--- SD1.5_FID.py
+++ SD1.5_FID.py
@@ -141,7 +141,7 @@ for T in step_sizes:
                 cuda=True,
                 fid=True,
                 verbose=False,
-                save_cpu_ram=True # 메모리 절약
+                save_cpu_ram=True,
                 samples_resize=299
             )
             fid = metrics['frechet_inception_distance']
